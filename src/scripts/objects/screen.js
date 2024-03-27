e0d17945e4339be02c73e99ab02ee74f3b2078c9
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-27 15:50:35 -0300

    Atualizar renderização do perfil do usuário e adicionar atividades

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,12 +1,12 @@
 const screen = {
-    userProfile: document.querySelector('.profile-data'),
-    renderUser(user){
-        this.userProfile.innerHTML = `
+  userProfile: document.querySelector(".profile-data"),
+  renderUser(user) {
+    this.userProfile.innerHTML = `
         <div class="info">
             <img src="${user.avatarUrl}" alt="Foto do perfil do usuário.">
             <div class="data">
-                <h1>${user.name ?? 'Não possui nome cadastrado 😭'}</h1>
-                <p>${user.bio ?? 'Não possui bio cadastrada 😭'}</p>
+                <h1>${user.name ?? "Não possui nome cadastrado 😭"}</h1>
+                <p>${user.bio ?? "Não possui bio cadastrada 😭"}</p>
                 <hr>
                 <div class="social">
                     <div class="follow followers">
@@ -19,61 +19,65 @@ const screen = {
                     </div>
                 </div>
             </div>
-        </div>`
+        </div>`;
 
-        let repositoriesItems = ''
-        user.repositories.forEach(repo => repositoriesItems += `
+    let repositoriesItems = "";
+    user.repositories.forEach(
+      (repo) =>
+        (repositoriesItems += `
         <li>
             <a href="${repo.html_url}" target="_blank">${repo.name}<br>
                 <span>
                     <p>🍴${repo.forks}</p>
                     <p>⭐${repo.stargazers_count}</p>
                     <p>👀${repo.watchers}</p>
-                    <p>🧑🏻‍💻${repo.language ?? 'Sem Linguagem :('}</p>
+                    <p>🧑🏻‍💻${repo.language ?? "Sem Linguagem :("}</p>
                 </span>
             </a>
         </li>`)
+    );
 
-        if(user.repositories.length > 0){
-            this.userProfile.innerHTML += `
+    if (user.repositories.length > 0) {
+      this.userProfile.innerHTML += `
             <div class="repositories section">
                 <hr>
                 <h2>Repositórios</h2>
                 <ul>${repositoriesItems}</ul>
                 <hr>
-            </div>`
-        }
+            </div>`;
+    }
 
-        document.querySelector('.profile-data').innerHTML += `
+    document.querySelector(".profile-data").innerHTML += `
         <div class="activityInfo">
             <h2 id="activityTittle">Atividades</h2>
-        </div>`
+        </div>`;
 
-        user.activities.forEach( repoName => {
-            const repoNOME = repoName.repo.name
-            
-            if(repoName.payload){
-                if(repoName.payload.commits){
-                    repoName.payload.commits.forEach( msg => {
-                        const commitMSG = msg.message
-                        
-                        document.querySelector('.profile-data').innerHTML += `
+    user.activities.forEach((repoName) => {
+      const repoNOME = repoName.repo.name;
+
+      if (repoName.payload) {
+        if (repoName.payload.commits) {
+          repoName.payload.commits.forEach((msg) => {
+            const commitMSG = msg.message;
+
+            document.querySelector(".profile-data").innerHTML += `
                         <span class="activityListClass">
                             <ul>
                                 <li>
                                     <p><strong>${repoNOME}</strong>: ${commitMSG}</p>
                                 </li>
                             </ul>
-                        </span>`
-                    })
-                }
-            }
-        })
-    },
+                        </span>`;
+          });
+        }
+      }
+    });
+  },
 
-    renderNotFound(){
-        this.userProfile.innerHTML = '<h3>[ERRO] Usuário não encontrado <i class="fa-solid fa-bug"></i></h3>'
-    }
-}
+  renderNotFound() {
+    this.userProfile.innerHTML =
+      '<h3>[ERRO] Usuário não encontrado <i class="fa-solid fa-bug"></i></h3>';
+  },
+};
 
-export {screen}
+export { screen };
