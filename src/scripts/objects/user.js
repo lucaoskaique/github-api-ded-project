4aa0f0671f9252b25f4a8631bd1de8c8aec7e0d1
lucaoskaique <lucas.kaique175@hotmail.com>
2024-03-27 15:50:39 -0300

    Atualizar objeto de usuário

--- src/scripts/objects/user.js
+++ src/scripts/objects/user.js
@@ -1,28 +1,28 @@
 const user = {
-    avatarUrl: '',
-    name: '',
-    bio: '',
-    userName: '',
-    followers: '',
-    following: '',
-    repositories: [],
+  avatarUrl: "",
+  name: "",
+  bio: "",
+  userName: "",
+  followers: "",
+  following: "",
+  repositories: [],
 
-    activities: [],
+  activities: [],
 
-    setInfo(gitHubUser){
-        this.avatarUrl = gitHubUser.avatar_url
-        this.name = gitHubUser.name
-        this.bio = gitHubUser.bio
-        this.userName = gitHubUser.login
-        this.followers = gitHubUser.followers
-        this.following = gitHubUser.following
-    },
-    setRepositories(repositories){
-        this.repositories = repositories
-    },
-    setActivities(activities){
-        this.activities = activities
-    }
-}
+  setInfo(gitHubUser) {
+    this.avatarUrl = gitHubUser.avatar_url;
+    this.name = gitHubUser.name;
+    this.bio = gitHubUser.bio;
+    this.userName = gitHubUser.login;
+    this.followers = gitHubUser.followers;
+    this.following = gitHubUser.following;
+  },
+  setRepositories(repositories) {
+    this.repositories = repositories;
+  },
+  setActivities(activities) {
+    this.activities = activities;
+  },
+};
 
-export {user}
+export { user };
